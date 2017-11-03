ce5795a97c03617f179a92e19475581853a4dca2
Eduardo Gomes <edugomes@fb.com>
2017-11-03 17:10:15 -0200

    Refactoring

--- app/ChatbotHelper.php
+++ app/ChatbotHelper.php
@@ -3,6 +3,8 @@ namespace App;
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Dotenv\Dotenv;
+
 class ChatbotHelper
 {
     protected $facebookSend;
@@ -11,6 +13,8 @@ class ChatbotHelper
     public $config;
     public function __construct()
     {
+        $dotenv = new Dotenv(dirname(__FILE__, 2));
+        $dotenv->load();        
         $this->accessToken = getenv(['PAGE_ACCESS_TOKEN']);
         $this->facebookSend = new FacebookSend();
         $this->log = new Logger('general');
@@ -25,5 +29,39 @@ class ChatbotHelper
     public function send($senderId, string $replyMessage)
     {
         return $this->facebookSend->send($this->accessToken, $senderId, $replyMessage);
+    }
+
+    public function verify_token(){
+        $hub_mode = array_key_exists('hub_mode', $_GET) ? $_GET['hub_mode'] : '';
+        $hub_challenge = array_key_exists('hub_challenge', $_GET) ? $_GET['hub_challenge'] : '';
+        $hub_verify_token = array_key_exists('hub_verify_token', $_GET) ? $_GET['hub_verify_token'] : '';
+        
+        if ($hub_mode !== 'subscribe') {
+            $log->debug('Invalid webhook mode');
+            http_response_code(400);
+            die(0);
+        }
+        if ($hub_verify_token !== getenv('VERIFY_TOKEN')) {
+            $log->debug('Failed to verify token');
+            http_response_code(400);
+            die(0);
+        }
+        $log->info('Webhook subscribed');
+        echo($hub_challenge);
+        die(0);       
+    }
+    public function check_hub_signature(){
+        if (!array_key_exists('HTTP_X_HUB_SIGNATURE', $_SERVER)) {
+            $log->debug('X-Hub-Signature header not found');
+            http_response_code(400);
+            die(0);
+        }
+        $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        $expected = 'sha1=' . hash_hmac($body, getenv('APP_SECRET'), 'sha1');
+        if ($expected !== $signature) {
+            $log->debug('X-Hub-Signature does not match');
+            http_response_code(400);
+            die(0);
+        }    
     }
 }
--- index1.php
+++ index1.php
@@ -3,61 +3,18 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\ChatbotHelper;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
-use Dotenv\Dotenv;
-$dotenv = new Dotenv(dirname(__FILE__, 2));
-$dotenv->load();
 
 // Create the chatbot helper instance
 $chatbotHelper = new ChatbotHelper();
 $log = new Logger('general');
 $log->pushHandler(new StreamHandler('debug.log'));
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $hub_mode = array_key_exists('hub_mode', $_GET) ? $_GET['hub_mode'] : '';
-    $hub_challenge = array_key_exists('hub_challenge', $_GET) ? $_GET['hub_challenge'] : '';
-    $hub_verify_token = array_key_exists('hub_verify_token', $_GET) ? $_GET['hub_verify_token'] : '';
-  
-    if ($hub_mode !== 'subscribe') {
-        $log->debug('Invalid webhook mode');
-        http_response_code(400);
-        die(0);
-    }
-    if ($hub_verify_token !== getenv('VERIFY_TOKEN')) {
-        $log->debug('Failed to verify token');
-        http_response_code(400);
-        die(0);
-    }
-    $log->info('Webhook subscribed');
-    echo($hub_challenge);
-    die(0);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+    $chatbotHelper->verify_token();
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST") {    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {   
+    $chatbotHelper->check_hub_signature();
+
     $body = file_get_contents('php://input');
-
-    if (!array_key_exists('HTTP_X_HUB_SIGNATURE', $_SERVER)) {
-        $log->debug('X-Hub-Signature header not found');
-        http_response_code(400);
-        die(0);
-    }
-    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-    $expected = 'sha1=' . hash_hmac($body, getenv('APP_SECRET'), 'sha1');
-    if ($expected !== $signature) {
-        $log->debug('X-Hub-Signature does not match');
-        http_response_code(400);
-        die(0);
-    }
-
     $data = json_decode($body, true);
-    if (array_key_exists('entry', $data)) {
-        foreach ($data['entry'] as $entry) {
-            if (array_key_exists('messaging', $entry)) {
-                foreach ($entry['messaging'] as $item) {
-                    $senderId = $item->sender->id;
-                    $message = $item->message->text;
-                    $replyMessage = "Echo:" + $message;
-                    $chatbotHelper->send($senderId, $replyMessage);
-                }
-            }
-        }
-    }
+    $chatbotHelper->handle_msg($data);
 }
